7b675421afd7ea0a87866bc11d2a4b1d9b7acb3c
John Gualteros <gualterosjohn40@gmail.com>
2023-01-06 09:12:17 -0500

    Initial commit

---
+++ src/main/java/com/example/selenium/SeleniumApplication.java
@@ -0,0 +1,13 @@
+package com.example.selenium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeleniumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeleniumApplication.class, args);
+	}
+
+}
